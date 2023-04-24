d237a4cf7cf2f62529bd5a296df262c1153ef532
Kitana <kittoft@gmail.com>
2023-04-24 14:58:23 -0700

    adding visual overview

---
+++ leetcode/containsNearbyDuplicate.py
@@ -0,0 +1,16 @@
+# 219. Contains Duplicate II
+# Link: https://leetcode.com/problems/contains-duplicate-ii/
+
+## Description
+# Given an integer array `nums`` and an integer `k``, return true if there
+# are two distinct indices `i`` and `j`` in the array such that
+# `nums[i] == nums[j]` and `abs(i - j) <= k`.
+
+## Brute Force Method
+
+def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
