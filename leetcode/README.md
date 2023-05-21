0b5da7082f5fc9c505f30426f3287657a1a7fd08
Kitana <kittoft@gmail.com>
2023-05-21 16:06:35 -0700

    search 2d matrix using binary search

--- leetcode/README.md
+++ leetcode/README.md
@@ -40,10 +40,16 @@ Check out my portfolio on [Medium](https://medium.com/@kitanatoft) to see my bre
 
 ---
 
-## Binary
+## Binary Search
 
 - [x] [Binary Search](https://leetcode.com/problems/binary-search/)
 - [x] [Search a 2D Matrix](https://leetcode.com/problems/search-a-2d-matrix/)
+<!-- - [ ] []() -->
+
+---
+
+## Binary
+
 - [ ] [Sum of Two Integers](https://leetcode.com/problems/sum-of-two-integers/)
 - [ ] [Number of 1 Bits](https://leetcode.com/problems/number-of-1-bits/)
 - [ ] [Counting Bits](https://leetcode.com/problems/counting-bits/)
--- leetcode/binary_search/searchMatrix.py
+++ leetcode/binary_search/searchMatrix.py
@@ -1,23 +1,40 @@
 # Search a 2D Matrix
 # Link: https://leetcode.com/problems/search-a-2d-matrix/
 
-## Linear Search: O(m*n)
-def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == target: return True
-        return False
-
+## Linear Search -- Time Complexity: O(m*n)
 # def searchMatrix(self, matrix, target):
 #         """
 #         :type matrix: List[List[int]]
 #         :type target: int
 #         :rtype: bool
 #         """
-
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if matrix[i][j] == target: return True
 #         return False
+
+## Iterative Binary Search -- Time Complexity: O(logm) + O(logn) = O(log(m*n))
+def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rows = len(matrix)
+        cols = len(matrix[0])
+        start = 0
+        end = rows * cols - 1
+
+        while start <= end:
+            mid = start + (end - start) // 2
+            mid_val = matrix[mid / cols][mid % cols]
+
+            if mid_val > target:
+                end = mid - 1
+            elif mid_val < target:
+                start = mid + 1
+            else:
+                return True
+
+        return False
+
