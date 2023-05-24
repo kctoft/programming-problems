98937c5c3e880fc9fd878ae86e7c581f9ab5dc40
Kitana <kittoft@gmail.com>
2023-05-23 17:09:07 -0700

    reverse a linked list: success

--- leetcode/linked-list/reverseList.py
+++ leetcode/linked-list/reverseList.py
@@ -9,11 +9,11 @@ def reverseList(self, head):
         # Initialize prev pointer as NULL
         prev = None
         # Initialize the cur pointer as the head
-        curr = head
+        cur = head
         # Run a loop till cur points to NULL
         while cur:
             # Initialize next pointer as the next pointer of cur
-            next = curr.next
+            next = cur.next
             # Now assign the prev pointer to cur’s next pointer
             cur.next = prev
             # Assign cur to prev, next to cur
